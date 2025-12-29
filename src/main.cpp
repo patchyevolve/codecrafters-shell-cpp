047acd309a12cbf7a19d8112957269636cb33b92
patchyevolve <ishandaksh1000@gmail.com>
2025-12-29 15:15:38 +0530

    1.2

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,20 @@ int main() {
     else if (cmd.substr(0,4) == "echo"){
       std::cout << cmd.substr(5) << std::endl;
     }
+    else if (cmd.substr(0,4) == "type"){
+      if(cmd.substr(5) == "echo"){
+        std::cout << cmd.substr(5) << " is a shell builtin" << std::endl;
+      }
+      else if(cmd.substr(5) == "exit"){
+        std::cout << cmd.substr(5) << " is a shell builtin" << std::endl;
+      }
+      else if(cmd.substr(5) == "type"){
+        std::cout << cmd.substr(5) << " is a shell builtin" << std::endl;
+      }
+      else{
+        std::cout << cmd.substr(5) << ": not found" << std::endl;
+      }
+    }
     else{
         std::cout << cmd << ": command not found" << std::endl ;
     }
