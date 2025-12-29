6d4e0b024161d1cba61e854c9433cc2f1b7a2f3a
patchyevolve <ishandaksh1000@gmail.com>
2025-12-29 15:09:48 +0530

    1.2

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,18 @@ int main() {
 
   while(true){
     std::cout << "$ ";
+    
     std::string cmd;
     std::getline(std::cin , cmd);
-    if(cmd == "exit"){ break;}
+
+    if(cmd == "exit"){ 
+      break;
+    }
+    else if (cmd.substr(0,4) == "echo"){
+      std::cout << cmd.substr(5) << std::endl;
+    }
     else{
-      std::cout << cmd << ": command not found" << std::endl ;
+        std::cout << cmd << ": command not found" << std::endl ;
     }
   }
   
